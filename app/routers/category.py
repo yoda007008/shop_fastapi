c0c884c65eda79d3194e75d3e801b78441f82e56
Кирилл <masteryoda01010101@gmail.com>
2024-08-03 12:40:20 +0300

    new requests in category and main

---
+++ app/routers/category.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter
+
+router = APIRouter(prefix="/category", tags=["category"])
+
+@router.get("/all_categories")
+async def get_all_categories():
+    pass
+
+@router.post("/create")
+async def create_category():
+    pass
+
+@router.put("/update_category")
+async def update_category():
+    pass
+
+@router.delete("/delete")
+async def delete_category():
+    pass
--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello FastAPI")
+from fastapi import FastAPI
+from app.routers import category
+
+app = FastAPI()
+
+@app.get("/")
+async def welcome() -> dict:
+    return {"message": "My e-commerse app"}
+
+app.include_router(category.router)
