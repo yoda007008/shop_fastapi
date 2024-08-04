a062334e0911d360e31f90ec53d22f2f13a8e982
Кирилл <masteryoda01010101@gmail.com>
2024-08-04 18:14:12 +0300

    new model in products.py

--- app/routers/products.py
+++ app/routers/products.py
@@ -1,6 +1,25 @@
 from fastapi import APIRouter
+from app.backend.db import Base
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean, Float
+from sqlalchemy.orm import relationship
 
 router = APIRouter(prefix='/products', tags=['products'])
+
+
+# new model
+class Product(Base):
+
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    slug = Column(String, unique=True, index=True)
+    description = Column(String)
+    price = Column(Integer)
+    image_url = Column(String)
+    stock = Column(Integer)
+    rating = Column(Float)
+    is_active = Column(Boolean, default=True)
 
 
 @router.get('/')
