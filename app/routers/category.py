dd290cfaf1b487fcf9c4351d37cbd35d17f7b623
Кирилл <masteryoda01010101@gmail.com>
2024-08-04 18:04:43 +0300

    new model in category.py

--- app/routers/category.py
+++ app/routers/category.py
@@ -1,6 +1,17 @@
 from fastapi import APIRouter
+from app.backend.db import Base
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
+from sqlalchemy.orm import relationship
 
 router = APIRouter(prefix="/category", tags=["category"])
+
+class Category(Base):
+    __tablename__ = "categories"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    slug = Column(String, unique=True, index=True)
+    is_active = Column(Boolean, default=True)
 
 @router.get("/all_categories")
 async def get_all_categories():
