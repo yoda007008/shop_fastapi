44d23b748360da95a6f63887fd27beb28f040515
Кирилл <masteryoda01010101@gmail.com>
2024-08-07 15:44:47 +0300

    upgrade second category.py

--- app/routers/category.py
+++ app/routers/category.py
@@ -1,4 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, status, HTTPException
+from sqlalchemy.orm import Session
+from app.backend.db_depends import get_db
+from typing import Annotated
+
+from app.models import *
+from sqlalchemy import insert
+from app.schemas import CreateCategory
+
+from slugify import slugify
 
 router = APIRouter(prefix='/category', tags=['category'])
 
@@ -9,9 +18,15 @@ async def get_all_categories():
 
 
 @router.post('/create')
-async def create_category():
-    pass
-
+async def create_category(db: Annotated[Session, Depends(get_db)], create_category: CreateCategory):
+    db.execute(insert(Category).values(name=create_category.name,
+                                       parent_id=create_category.parent_id,
+                                       slug=slugify(create_category.name)))
+    db.commit()
+    return {
+        'status_code': status.HTTP_201_CREATED,
+        'transaction': 'Successful'
+    }
 
 @router.put('/update_category')
 async def update_category():
